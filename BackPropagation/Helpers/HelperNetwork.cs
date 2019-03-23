5cf2a8569a352457f526391dc3e2d96e745fd45f
Fingann <Fingann92@gmail.com>
2019-03-23 11:03:19 +0100

    Made it run

--- BackPropagation/Helpers/HelperNetwork.cs
+++ BackPropagation/Helpers/HelperNetwork.cs
@@ -1,38 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Xml;
 using BackPropagation.Abstractions;
 using BackPropagation.ActivationFunctions;
-using BackPropagation.ActivationFunctions.Delegates;
 
 namespace BackPropagation.Helpers
 {
-	internal class HelperNetwork: NetworkBase<HelperNeuron>
-	{
-		public List<HelperSynapse> Synapses { get; set; }
+    internal class HelperNetwork : NetworkBase<HelperNeuron>
+    {
+        public HelperNetwork()
+        {
+            Synapses = new List<HelperSynapse>();
+        }
 
-		public HelperNetwork() 
-		{
-			Synapses = new List<HelperSynapse>();
-		}
-	}
+        public List<HelperSynapse> Synapses { get; set; }
+    }
 
-	internal class HelperNeuron : NeuronBase
-	{
-		public HelperNeuron(Guid id, float bias, float biasDelta, float gradient, float value, ActivationType type): base(id,bias,biasDelta,gradient,value,type)
-		{
-		}
-	}
+    internal class HelperNeuron : NeuronBase
+    {
+        public HelperNeuron(Guid id, float bias, float biasDelta, float gradient, float value, ActivationType type) :
+            base(id, bias, biasDelta, gradient, value, type)
+        {
+        }
+    }
 
-	internal class HelperSynapse:SynapseBase
-	{
-		public Guid OutputNeuronId { get; set; }
-		public Guid InputNeuronId { get; set; }
-	
-		public HelperSynapse(Guid id, float weight, float weightDelta,Guid outputNeuronId, Guid inputNeuronId): base(id,weight,weightDelta)
-		{
-			OutputNeuronId = outputNeuronId;
-			InputNeuronId = inputNeuronId;
-		}
-	}
+    internal class HelperSynapse : SynapseBase
+    {
+        public HelperSynapse(Guid id, float weight, float weightDelta, Guid outputNeuronId, Guid inputNeuronId) : base(
+            id, weight, weightDelta)
+        {
+            OutputNeuronId = outputNeuronId;
+            InputNeuronId = inputNeuronId;
+        }
+
+        public Guid OutputNeuronId { get; set; }
+        public Guid InputNeuronId { get; set; }
+    }
 }
